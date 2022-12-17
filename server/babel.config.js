3b632b560fe35203a77da3797c11acd37fce65c6
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-12-17 11:20:47 -0300

    server - initial setup

---
+++ server/babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    "@babel/preset-typescript",
+    ["@babel/preset-env", { targets: { node: "current" }, loose: false }],
+  ],
+  plugins: [
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: false }],
+    [
+      "module-resolver", { alias: {
+      }}
+    ],
+  ],
+  ignore: ["**/*.spec.ts"],
+};

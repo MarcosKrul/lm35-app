35e771a6791bf966a3b550ea1ec780486123e6d1
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-12-17 14:57:20 -0300

    server index - mqtt subscribe example

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,18 @@
+import { TopicsMQTT } from "@common/TopicsMQTT";
 import { env } from "@helpers/env";
 import { app } from "@infra/http/app";
+import { mqttClient } from "@infra/mqtt";
 
 const port = env("PORT");
 if (!port) throw new Error("No PORT configurated");
 
 app.listen(port, () => console.log(`Server started at ${port}`));
+
+mqttClient.subscribe({
+  [`${TopicsMQTT.RECEIVED_FROM_LM35}`]: {
+    qos: 2,
+    cb: (payload) => {
+      console.log(`Dados recebido do sensor LM35: ${payload.toString()}`);
+    },
+  },
+});

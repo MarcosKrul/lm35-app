c40dfd275451a62f158d00418de70d175f46e9b8
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-12-18 15:57:22 -0300

    embedded shetch main - mqtt setup

--- embedded/src/Arduino_IDE/sketch_main/sketch_main.ino
+++ embedded/src/Arduino_IDE/sketch_main/sketch_main.ino
@@ -1,17 +1,49 @@
+#include <PubSubClient.h>
 #include <WiFiConnection.h>
 
 #define WIFI_SSID "WIFI_SSID_HERE"
 #define WIFI_PASSWORD "WIFI_PASSWORD_HERE"
 
+#define MQTT_PORT MQTT_PORT_HERE
+#define MQTT_HOST "MQTT_HOST_HERE"
+#define MQTT_SECRET_HASH "MQTT_SECRET_HASH_HERE"
+
+void connectionsStatus();
+void onMQTTMessageCallback(char*,byte*,unsigned int);
+
+WiFiClient wiFiClient;
+PubSubClient mqttClient(wiFiClient);
 WiFiConnection wiFiConnection = WiFiConnection(WIFI_SSID, WIFI_PASSWORD);
 
 void setup() {
 	Serial.begin(9600);
+	randomSeed(analogRead(A0));
+
+	mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setCallback(onMQTTMessageCallback);
 }
 
 void loop() {
 	if (!wiFiConnection.connected()) wiFiConnection.reconnect();
+	else if (!mqttClient.connected()) {
+		mqttClient.connect("__MQTTClientId LM35-app" + random(300));
+		mqttClient.subscribe("/mqtt/engcomp/lm35/"MQTT_SECRET_HASH"/diffusion");
+	}
   
+	mqttClient.loop();
 	wiFiConnection.printStatus();
+
 	delay(50);
+}
+
+void onMQTTMessageCallback(char* topic, byte* payload, unsigned int size) {
+  Serial.println("MQTT Client received message at: ");
+  Serial.print(topic);
+
+  Serial.print(" / Message: ");
+  for (int i = 0; i < size; i++) {
+    Serial.print((char) payload[i]);
+  }
+
+  Serial.println();
 }

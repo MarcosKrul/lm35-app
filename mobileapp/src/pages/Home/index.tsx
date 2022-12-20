3294eb24f4ae28f636c57e6ede36a4e3173f822c
Renato Ruppel <heruppel2@gmail.com>
2022-12-19 23:06:56 -0300

    feat(mqtt): value reading and broker toggle

--- mobileapp/src/pages/Home/index.tsx
+++ mobileapp/src/pages/Home/index.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useMqtt } from '../../hooks/mqtt';
 import { styles } from './styles';
 
 const Home = (): JSX.Element => {
-  const { subscribe, unsubscribe, payload } = useMqtt();
+  const { subscribe, unsubscribe, payload, publish } = useMqtt();
+  const [analogValue, setAnalogValue] = useState<string>('0');
+  const [tempValue, setTempValue] = useState<string>('0');
+  const [brokerStopped, setBrokerStopped] = useState<boolean>(false);
 
   useEffect(() => {
     subscribe('/mqtt/engcomp/lm35/0a6e2389ec3fecd2a8068a0097ef5f96/diffusion', {
@@ -22,12 +25,53 @@ const Home = (): JSX.Element => {
   useEffect(() => {
     if (payload) {
       console.log(payload.toString());
+      const parsedValues: { temp: string; analog: string } = JSON.parse(
+        payload.toString(),
+      );
+
+      setAnalogValue(prev => parsedValues.analog ?? prev);
+      setTempValue(prev => parsedValues.temp ?? prev);
     }
   }, [payload]);
 
+  const getRawValue = (analog: number): string =>
+    `${((analog * 5.0) / 1024).toFixed(2)}`;
+
+  const toggleMessages = (): void => {
+    publish(
+      '/mqtt/engcomp/lm35/0a6e2389ec3fecd2a8068a0097ef5f96/control/toggle',
+      '',
+      { qos: 2 },
+    );
+    setBrokerStopped(prev => !prev);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Bem-vindo(a)</Text>
+      <View style={styles.header}>
+        <Text style={styles.welcome}>Bem-vindo</Text>
+        <TouchableOpacity onPress={toggleMessages}>
+          <Text style={styles.toggle}>
+            {brokerStopped ? 'Ligar' : 'Desligar'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.valuesContainer}>
+        <View style={styles.analogContainer}>
+          <Text style={styles.analogLabel}>Valor de tensão</Text>
+          <Text style={styles.value}>{`${getRawValue(
+            Number(analogValue),
+          )} V`}</Text>
+        </View>
+        <View style={styles.analogContainer}>
+          <Text style={styles.analogLabel}>Valor analógico</Text>
+          <Text style={styles.value}>{`${analogValue}`}</Text>
+        </View>
+        <View style={styles.tempContainer}>
+          <Text style={styles.tempLabel}>Temperatura</Text>
+          <Text style={styles.value}>{`${tempValue} °C`}</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- mobileapp/src/routes/app.routes.tsx
+++ mobileapp/src/routes/app.routes.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../pages/Home';
-import { colors } from '../global/colors';
 
 export type StackAppParams = {
   Home: {
@@ -17,11 +16,7 @@ const AppRoutes = (): JSX.Element => {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          //headerShown: false,
-          headerTintColor: '#FFF',
-          headerStyle: {
-            backgroundColor: colors.PRIMARY,
-          },
+          headerShown: false,
           animation: 'fade',
         }}>
         <Stack.Screen name="Home" component={Home} />

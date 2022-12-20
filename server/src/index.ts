2783a63d4204fb3af450d0464a3b769e80f4f033
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-12-19 22:27:28 -0300

    server - handle mqtt services errors

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,10 @@ import "@containers/index";
 import { container } from "tsyringe";
 
 import { TopicsMQTT } from "@common/TopicsMQTT";
+import { AppError } from "@handlers/errors/AppError";
 import { env } from "@helpers/env";
 import { app } from "@infra/http/app";
+import { logger } from "@infra/log";
 import { mqttClient } from "@infra/mqtt";
 import { HandleLM35ReceivedDataService } from "@services/mqtt";
 
@@ -15,9 +17,14 @@ if (!port) throw new Error("No PORT configurated");
 
 app.listen(port, () => console.log(`Server started at ${port}`));
 
-mqttClient.subscribe({
-  [`${TopicsMQTT.RECEIVED_FROM_LM35}`]: {
-    qos: 2,
-    cb: container.resolve(HandleLM35ReceivedDataService).cb,
-  },
-});
+try {
+  mqttClient.subscribe({
+    [`${TopicsMQTT.RECEIVED_FROM_LM35}`]: {
+      qos: 2,
+      cb: container.resolve(HandleLM35ReceivedDataService).cb,
+    },
+  });
+} catch (e) {
+  if (e instanceof AppError) logger.error(e.message);
+  else logger.error("Unknown error on MQTT message callback");
+}

b9a7ffccd0bb2c787a0e90cf06689732ad6e0e6f
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-12-20 19:54:08 -0300

    embedded sketch main - custom lcd char & time fixed

--- embedded/src/Arduino_IDE/sketch_main/sketch_main.ino
+++ embedded/src/Arduino_IDE/sketch_main/sketch_main.ino
@@ -28,6 +28,7 @@
 #define LCD_ROWS 2
 #define LCD_COLUMNS 16
 #define LCD_DISPLAY_VALUES_IN_MS 1000
+#define LCD_FIRST_FREE_COLUMN 3
 
 #define GET_CELSIUS_BY_RAW_VALUE(value) (value * 5.0 / (pow(2, ADC_RESOLUTION)-1)) / 0.01
 
@@ -42,7 +43,7 @@ byte last_state_blink_mqtt_led = 1;
 unsigned long last_millis_blink_mqtt_led = millis();
 unsigned long last_displayed_from_lcd = millis();
 unsigned long last_publish_lm35data = millis();
-unsigned long time_to_publish = 0;
+unsigned long time_to_publish = 1000;
 const char* title_temp = "Temp: ";
 const char* title_analog = "Analog: ";
 
@@ -70,17 +71,45 @@ byte custom_degrees_char[8] = {
 	0b00000000,
 };
 
+byte custom_temp_char[4][8] = {
+	{
+		0b00001, 0b00011, 0b00011, 0b00111,
+    0b01111, 0b01111, 0b01111, 0b11111
+	},
+	{
+		0b10000, 0b11000, 0b11000, 0b11100,
+    0b11110, 0b11110, 0b11110, 0b11111
+	},
+	{
+		0b11111, 0b11111, 0b11100, 0b11100,
+    0b11100, 0b11100, 0b11100, 0b11100
+	},
+	{
+		0b11111, 0b11111, 0b11111, 0b10001,
+    0b10001, 0b11111, 0b11111, 0b11111
+	},
+};
+
 void setup() {
 	Serial.begin(9600);
 	randomSeed(analogRead(A0));
 
 	lcd.begin(LCD_COLUMNS, LCD_ROWS);
 	lcd.createChar(0, custom_degrees_char);
+	lcd.createChar(1, custom_temp_char[0]);
+	lcd.createChar(2, custom_temp_char[1]);
+	lcd.createChar(3, custom_temp_char[2]);
+	lcd.createChar(4, custom_temp_char[3]);
 
-	lcd.setCursor(0, 0);
+	lcd.setCursor(0, 0); lcd.write((byte) 1);
+	lcd.setCursor(1, 0); lcd.write((byte) 2);
+	lcd.setCursor(0, 1); lcd.write((byte) 3);
+	lcd.setCursor(1, 1); lcd.write((byte) 4);
+
+	lcd.setCursor(LCD_FIRST_FREE_COLUMN, 0);
 	lcd.print(title_temp);
 	
-	lcd.setCursor(0, 1);
+	lcd.setCursor(LCD_FIRST_FREE_COLUMN, 1);
 	lcd.print(title_analog);
 
   pinMode(PIN_LM35, INPUT);
@@ -98,12 +127,12 @@ void loop() {
 	tempConverted = GET_CELSIUS_BY_RAW_VALUE(analogReadFromLM35);
 
 	if (millis() > (last_displayed_from_lcd + LCD_DISPLAY_VALUES_IN_MS)) {
-		lcd.setCursor(length_char(title_temp), 0);
+		lcd.setCursor(length_char(title_temp) + LCD_FIRST_FREE_COLUMN, 0);
 		lcd.print(tempConverted, 1);
 		lcd.write((byte) 0);
 		lcd.print("C ");
 		
-		lcd.setCursor(length_char(title_analog), 1);
+		lcd.setCursor(length_char(title_analog) + LCD_FIRST_FREE_COLUMN, 1);
 		lcd.print(analogReadFromLM35, 1);
 
 		last_displayed_from_lcd = millis();
